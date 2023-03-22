1e89f4f7072212a190206da9eb3014ec238fd7a7
Kaye Bacos Bearneza <124229077+Kayiichin@users.noreply.github.com>
2023-03-22 23:47:28 +0800

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,30 @@
+public class SelectionSort{
+	public static void main(String[] args) {
+		
+		int array[] = {-2, 45,};
+		
+		selectionSort(array);
+		
+		for(int i : array) {
+			System.out.print(i);
+		}	
+	}
+
+	private static void selectionSort(int[] array) {
+		
+		for(int i = 0; i < array.length - 1; i++) {
+			int min = i;
+			for(int j = i + 1; j < array.length; j++) {
+				if(array[min] > array[j]) {
+					min = j;
+				}
+			}
+			
+			int temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+		}
+		
+	}
+}
+
